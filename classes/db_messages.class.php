0c0917438f5550e392933eed4ea7152843de89fe
Aleks <aleks.jevtic315@gmail.com>
2021-01-17 19:15:05 +0100

    fixed a few bugs around the nav bar, database n everything else. also about to create the messenger

--- classes/db_messages.class.php
+++ classes/db_messages.class.php
@@ -1,5 +1,56 @@
 <?php
 class db_messages extends Db_con{
   
+  function get_messages_list($user_array)
+  {
+    $con = $this->connect();
+    //ALL Messages involved with the User 
+    $query = "SELECT * FROM messages WHERE to_id = ? OR from_id= ?";
+    $stmt = $con->prepare($query);
+    $stmt->execute([$user_array['person_id'],$user_array['person_id']]);
+    $msg_array = array();
+    $person_id = $user_array['person_id'];
+    while($row = $stmt->fetch())
+    {
+      
+      $from_id =$row['from_id'];
+      $to_id = $row['to_id'];
+      //user received msg
+      if($person_id == $to_id)
+      {
+        if(isset($msg_array["$from_id"]))
+        {
+          $prev_message = $msg_array["$from_id"];
+          if($prev_message['msg_id']<$row['msg_id'])
+          {
+            $msg_array["$from_id"] = $row;
+            if(!$row['viewed'])
+            {
+              if(isset($msg_array["$from_id"]["unread_msg_total"]))
+              {
+                $msg_array["$from_id"]["unread_msg_total"]++;
+              }
+              else{
+                $msg_array["$from_id"]["unread_msg_total"]=1;
+              }
+            }
+          }
+        }
+      else{
+          $msg_array["$from_id"]=$row;
+      }
+    }
+    //user sent msg
+    else{
+      if(isset($msg_array["$to_id"]))
+      {
+        if($msg_array["$to_id"]["msg_id"]<$row["msg_id"])
+        {
+          $msg_array["$to_id"] = $row;
+        }
+      }
+    }
+  }
+  return $msg_array;
 }
-?>
+}
