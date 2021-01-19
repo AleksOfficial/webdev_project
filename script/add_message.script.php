996db554ae08b5ff4b94425c5b1d06334cdf4d03
Aleks <aleks.jevtic315@gmail.com>
2021-01-19 01:59:46 +0100

    Necessary changes to the classes to simplify the code / enable the chat function. We are almost there lads :)

---
+++ script/add_message.script.php
@@ -0,0 +1,16 @@
+<?php
+//check if ppl are friends & if user is logged
+if($_SESSION['logged'])
+{
+  $user_id = $_SESSION['user']['person_id'];
+  $recipient_id = $_GET['chat'];
+  $db_user = new Db_user();
+  if($db_user->check_friends($user_id,$recipient_id))
+  {
+    if(!empty($_POST['content']))
+      $content = $_POST['content'];
+      $db_messages->insert_message($user_id,$recipient_id,$content);
+
+  }
+
+}
