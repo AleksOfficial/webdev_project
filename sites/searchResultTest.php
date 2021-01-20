c55b9cd0093dd40329936a39943df9855e6219dd
Tribun Krasniqi <tribunkrasniqii@gmail.com>
2021-01-20 18:19:52 +0100

    searchuser is drinn und javascript in der index

--- sites/searchResultTest.php
+++ sites/searchResultTest.php
@@ -24,10 +24,8 @@ foreach($searchUser as $user)
 ?>
 </div>
 <?php
-if ($searchUser[0]['username'] != NULL) {
+if ($searchAmount > 0) {
     
-    $_SESSION['searchAmount'] = count($searchUser['username'], 0);
-    echo "testamount=" . $_SESSION['searchAmount'];
     
 ?>  <div class="container">     
         <h3>
@@ -36,7 +34,7 @@ if ($searchUser[0]['username'] != NULL) {
     </div>                                                              <?php
 
 
-} else /*if ($searchUser['username'] != NULL)*/ {            ?>
+} else if ($searchAmount == 0) {            ?>
     <div class="container">     
         <h3>
             <?php  echo "there are no results for \"" . $_GET['searchValue'] . "\"";   ?>
@@ -44,121 +42,75 @@ if ($searchUser[0]['username'] != NULL) {
     </div>                                              <?php
 }
 
-
-foreach ($searchUser as $test) {
-    echo "this is a test: " . $test;
-}
-
-//echo "testest " . $_SESSION['searchUser'];
-
-
-
-
 ?>
 
-
-
-<div class="container">
-    <div>
-        <h3>
-            <?php  echo "showing results for \"" . $_GET['searchValue'] . "\"";   ?>
-        </h3>
-    </div>
-   
 
     <div class="row">
 
 
             <?php
 
-                $rows = $_SESSION['searchAmount'] / 3;
-                $rowsRest = $_SESSION['searchAmount'] % 3;
+                $rows = $searchAmount / 3;
+                $rowsRest = $searchAmount % 3;
                 $userCount = 0;
-                for($y = 0 ; $y < 3 ; $y++) {
-?>                  <div class="col-md-4 searchResultContainer">                                        <?php
+                if ($searchAmount == 2) {
+                    for ($i = 0 ; $i < 2 ; $i++) {
+    ?>                  <div class="col-md-4 searchResultContainer">                                            <?php
 
-                    if ($rowsRest > 0) {
-                        for($i = 0 ; $i < $rows+1 ; $i++) {
-?>                          <div class="card searchResultCard" style="width: 100%;">         
-                                <img src="res/pics/narutotest.jpeg" class="card-img-top" alt="good shit">   
-                                <div class="card-body">
-                                    <h5 class="card-title">trylikeabun</h5>                 
-                                    <p class="card-text">Naruto best anime ever no doubt everyone can shut the fuck up.</p>
-                                    <a href="#" class="btn btn-primary">Visit profile</a>
-                                </div>
-                            </div>                                                                      <?php
-                            
+                            $searchUserClass->print_result_card($searchUser[$userCount]['person_id']);
                             $userCount++;
-                        }
-                    } else {
-                        for($i = 0 ; $i < $rows ; $i++) {
-?>                          <div class="card searchResultCard" style="width: 100%;">         
-                                <img src="res/pics/narutotest.jpeg" class="card-img-top" alt="good shit">   
-                                <div class="card-body">
-                                    <h5 class="card-title">trylikeabun</h5>                 
-                                    <p class="card-text">Naruto best anime ever no doubt everyone can shut the fuck up.</p>
-                                    <a href="#" class="btn btn-primary">Visit profile</a>
-                                </div>
-                            </div>                                                                      <?php
-                            
-                            $userCount++;
-                        }
+                    
+    ?>                  </div>                                                                                  <?php
                     }
-                    $rowsRest--;
+                    ?> <div class="col-md-4 searchResultContainer"></div> <?php
+                } else if ($searchAmount == 1) {
+                   
+?>                  <div class="col-md-4 searchResultContainer">                                            <?php
+                    
+                        $searchUserClass->print_result_card($searchUser[$userCount]['person_id']);
+                        $userCount++;
+                                        
+?>                  </div>                                                                                  <?php
+                                        
+                    ?> <div class="col-md-4 searchResultContainer"></div> <?php
+                    ?> <div class="col-md-4 searchResultContainer"></div> <?php
+                } else if ($searchAmount == 0) {
+                    ?>  bruh  <?php
+                } else if ($searchAmount >= 3) {
 
-?>                  </div>                                                                               <?php
-                }   
+
+
+                    for($y = 0 ; $y < 3 ; $y++) {
+    ?>                  <div class="col-md-4 searchResultContainer">                                        <?php
+                        // if statement falls weniger als 3 hasen
+
+                       
+                    
+                        if ($rowsRest > 0) {
+                            for($i = 0 ; $i < $rows+1 ; $i++) {
+                                $searchUserClass->print_result_card($searchUser[$userCount]['person_id']);
+                                
+                                $userCount++;
+                            }
+                        } else {
+                            for($i = 0 ; $i < $rows ; $i++) {
+                                $searchUserClass->print_result_card($searchUser[$userCount]['person_id']);
+                                
+                                $userCount++;
+                            }
+                        }
+                        $rowsRest--;
+
+    ?>                  </div>                                                                               <?php
+                    }   
+                }
                 
             ?>
     </div>
 </div>
 
 
- <!--       <div class="col-md-4 searchResultContainer">
 
-
-
-
-
-
-            <div class="card searchResultCard" style="width: 100%;">
-                <img src="res/pics/narutotest.jpeg" class="card-img-top" alt="good shit">
-                <div class="card-body">
-                    <h5 class="card-title">trylikeabun</h5>
-                    <p class="card-text">Naruto best anime ever no doubt everyone can shut the fuck up.</p>
-                    <a href="#" class="btn btn-primary">Visit profile</a>
-                    
-
-                </div>
-            </div>
-
-
-
-        </div>
-        <div class="col-md-4 searchResultContainer">
-            <div class="card searchResultCard" style="width: 100%;">
-                <img src="res/pics/narutotest.jpeg" class="card-img-top" alt="good shit">
-                <div class="card-body">
-                    <h5 class="card-title">trylikeabun</h5>
-                    <p class="card-text">Naruto best anime ever no doubt everyone can shut the fuck up.</p>
-                    <a href="#" class="btn btn-primary">Visit profile</a>
-                    
-
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 searchResultContainer">
-            <div class="card searchResultCard" style="width: 100%;">
-                <img src="res/pics/narutotest.jpeg" class="card-img-top" alt="good shit">
-                <div class="card-body">
-                    <h5 class="card-title">trylikeabun</h5>
-                    <p class="card-text">Naruto best anime ever no doubt everyone can shut the fuck up.</p>
-                    <a href="#" class="btn btn-primary">Visit profile</a>
-                    
-
-                </div>
-            </div>
-        </div> -->
     </div>
 
 </div>
