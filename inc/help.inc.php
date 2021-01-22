35708f4861bd4c4cfe5e1e6732ed7911aeb3567b
Tribun Krasniqi <tribunkrasniqii@gmail.com>
2021-01-22 02:11:07 +0100

    helppage

---
+++ inc/help.inc.php
@@ -0,0 +1,72 @@
+<!-- schlichtes Impressum lol :) -->
+
+<section id="help">
+
+    
+    <div class="container impressumContainer">
+    
+        <h3>
+            Hilfe
+        </h3>
+        <p>
+            Wenn Sie neu bei RIFT sind und sich noch nicht mit der Benutzung dieser social media-Plattform bekannt gemacht haben, dann sind Sie hier genau richtig!
+        </p>
+
+        <br>
+
+        <p>
+            Um alle Funktionen von RIFT nutzen zu können, müssen Sie sich zuerst registrieren. Dazu klicken Sie auf "Sign Up" in der Navigationsleiste, welche sich immer oben auf Ihrem Bildschirm befindet. Nun können Sie ihre Daten eintippen und einsenden, wonach Sie sich direkt mit diesen Daten unter "Login" bei der Navigationsleiste anmelden können.
+        </p>
+
+        <br>
+
+        <p>
+            Das Wichtigste ist getan! Sie können nun RIFT in vollem Ausmaß verwenden und sollten bereits mehrere neue Punkte in der Navigationsleiste sehen.
+        </p>
+
+        <br>
+
+        <p>
+            Auf der Startseite sehen Sie den Feed mit Beiträgen, welche sie liken, disliken und kommentieren können. Sie können auch gerne eigene Posts mit dem Formular im Feed erstellen.
+        </p>
+
+        <br>
+
+        <p>
+            Unter "Profile" können Sie Ihr Profil mit Ihren eigenen posts einsehen und gegebenenfalls Ihre Daten zum Profil ändern.  
+        </p>
+
+        <br>
+
+        <p>
+            Unter "Notifications" sehen Sie alle Benachrichtigungen für ihr Profil, so wie Likes, Kommentare, Freundschaftsanfragen etc.
+        </p>
+
+        <br>
+
+        <p>
+            Bei "Messages" können Sie sich mit anderen Personen aus RIFT unterhalten. Die letzten Chats werden Ihnen angezeigt, was Ihnen dabei hilft, vergangene Konversationen wieder aufzunehmen. 
+        </p>
+
+        <br>
+
+        <p>
+            Zu guter Letzt haben Sie noch den "Logout-Button" mit dem Sie sich von RIFT abmelden können und das Impressum.
+        </p>
+
+        <br>
+
+        <p>
+            Mit der Suchleiste können Sie nach speziellen Profilen, Posts, Kommentaren und Bildnamen suchen. Dadurch lassen sich andere Profile besuchen, denen Sie eine Freundschaftsanfrage senden können und Nachrichten schicken können.
+        </p>
+
+        <br>
+
+        <p>
+            Viel Spaß in RIFT!
+        </p>
+
+        
+</div>
+
+</section>
